3831d8be1c9e07c43d2c55f2128d739ebba3820c
paula-adriana <paulaadrianadacosta@gmail.com>
2024-04-03 16:33:07 -0300

    Composição exercicio 01 - primeira parte

---
+++ src/composicao/Program.java
@@ -0,0 +1,31 @@
+package composicao;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+//		Departament name = new Departament(null);
+//		Worker worker = new Worker(null, null, null);
+//		
+//		Scanner entrada = new Scanner(System.in);
+//		
+//		System.out.print("Enter departament's name: ");
+//		name.setName(entrada.next());
+//		
+//		System.out.println("Enter worker data: ");
+//		
+//		System.out.print("Name: ");
+//		worker.setName(entrada.next());
+//		
+//		System.out.print("Level: ");
+//		WorkerLevel.valueOf(entrada.next());
+//		
+//		System.out.print("Base salary: ");
+//		worker.setBaseSalary(entrada.nextDouble());
+		
+		
+		
+	}
+}
---
+++ src/composicao/Worker.java
@@ -0,0 +1,80 @@
+package composicao;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Iterator;
+import java.util.List;
+
+public class Worker {
+	
+	private String name;
+	private WorkerLevel level;
+	private Double baseSalary;
+	
+	//composição de objetos - associações
+	private Departament departament;
+	private List<HourContract> contracts = new ArrayList<HourContract>();
+	
+	public Worker() {}
+	
+	public Worker(String name, WorkerLevel workLevel, Double baseSalary, Departament departament) {
+		this.name = name;
+		this.level = workLevel;
+		this.baseSalary = baseSalary;
+		this.departament = departament;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public WorkerLevel geLevel() {
+		return level;
+	}
+	public void setWorkLevel(WorkerLevel level) {
+		this.level = level;
+	}
+	public Double getBaseSalary() {
+		return baseSalary;
+	}
+	public void setBaseSalary(Double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+	
+	public Departament getDepartament() {
+		return departament;
+	}
+	
+	public void setDepartament(Departament departament) {
+		this.departament = departament;
+	}
+	
+	public List<HourContract> getContracts() {
+		return contracts;
+	}
+
+	public void addContract (HourContract contract) {
+		contracts.add(contract);
+	}
+	
+	public void removeContract (HourContract contract) {
+		contracts.remove(contract);
+	}
+	
+	//salario base + o results do mes/ano do contrato daquele mes
+	public Double income (Integer year, Integer month) {
+		double sum = baseSalary;
+		//setar um calendario falso para comparaçao posterior
+		Calendar cal = Calendar.getInstance();
+		for (HourContract c : contracts) {
+			cal.setTime(c.getDate());
+			int c_year = cal.get(Calendar.YEAR);
+			int c_month = 1 + cal.get(Calendar.MONTH); //colocar 1 pois month começa em 0
+			if (year == c_year && month == c_month)
+				sum += c.totalValue();
+		}
+		return sum; 
+	}
+	
+}
